4cfbf653e81774a32713debc786231890b4cf2f2
Niklas Widmann <nik@somethingcatchy.net>
2022-09-16 09:08:30 +0200

    update util

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,5 @@
 import crypto from 'crypto'
 
-export function fileHash(content: Buffer, type = 'sha256') {
+export function fileHash(content: Buffer | string, type = 'sha256') {
    return crypto.createHash(type).update(content).digest('hex')
 }
